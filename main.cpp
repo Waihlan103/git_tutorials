b99afabc767d51ec41fb171a3434a7518e521218
Waihlan Kaung <waihlan103@gmail.com>
2024-12-02 21:41:47 +0630

    paid version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "paid.hpp"
 
 int main(){
      std::cout<<"Hello World\n";
      std::cout<<"Nay Kg lrr\n";
+     std::cout<<"calculate = "<<calculate(2, 4)<<std::endl;
 
      return 0;
 }
