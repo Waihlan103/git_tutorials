4aa80fc1f2311f268edeca97df8ec13fa6b6a95b
Waihlan Kaung <waihlan103@gmail.com>
2024-12-02 21:15:03 +0630

    add free.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "include/free.hpp"
+
+using namespace std;
 
 int main(){
+
      std::cout<<"Hello World\n";
+     std::cout<<"sum = "<<sum(2,3)<<std::endl;
 
      return 0;
 }
